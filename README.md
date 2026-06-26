379ae09a8e0826fa449b5bf23c5a845a0130d6a2
Zentifyyy <109432346+Zentifyyy@users.noreply.github.com>
2026-06-26 21:47:57 +0100

    Project Files
    FIX: Process not closing

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # DreadHungerServerGUI
 DreadHungerServerGUI is a python application created with Dear Pygui to give the user a GUI for hosting Dread Hunger servers on their machine.
+
+## Notes
+- DreadHungerServerGUI uses the [Stack Sans](https://fonts.google.com/specimen/Stack+Sans+Text?categoryFilters=Feeling:%2FExpressive%2FBusiness&preview.script=Latn) font, under the [Open Font License](https://openfontlicense.org/open-font-license-official-text/).
---
+++ script.py
@@ -0,0 +1,166 @@
+import dearpygui.dearpygui as dpg
+from tkinter import filedialog
+import asyncio
+import os
+
+WindowWidth = 775
+WindowHeight = 347
+
+class Globals():
+    ServerProc = asyncio.subprocess.Process
+    ServerPath = ""
+    LogPath = ""
+    HasOpenedBefore = False
+    ConsoleText = ""
+    MapCode = "Expanse_Persistent"
+    DaysUntilBlizzard = 3
+    DayMinutes = 8
+    MaxPlayers = 8
+    ThrallCount = 3
+    PredatorDamage = 1
+    CoalBurnRate = 1
+    ColdIntensity = 1
+    HungerRate = 1
+
+async def kill_process():
+    Globals.ServerProc.kill()
+    await Globals.ServerProc.wait()
+
+def save_prefs():
+    if Globals.ServerProc != asyncio.subprocess.Process:
+        asyncio.run(kill_process)
+    Globals.HasOpenedBefore = True
+    with open("prefs.ini", 'w') as f:
+        f.write(f"{Globals.ServerPath}\n{Globals.LogPath}\n{Globals.HasOpenedBefore}")
+        f.close()
+
+with open("prefs.ini",mode='r') as f:
+    content = f.read().splitlines()
+    try:
+        if os.path.exists(content[0]):
+            Globals.ServerPath = content[0]
+        if os.path.exists(content[1]):
+            Globals.LogPath = content[1]
+        Globals.HasOpenedBefore = content[2] == "True"
+    except: f.close()
+    f.close()
+
+dpg.create_context()
+dpg.create_viewport(title='Dread Hunger Server', width=WindowWidth, height=WindowHeight)
+dpg.set_exit_callback(save_prefs)
+dpg.configure_app( docking=True, docking_space=True, init_file="windows.ini")
+
+with dpg.font_registry():
+    default_font = dpg.add_font("assets/StackSansText-ExtraLight.ttf", 13)
+    dpg.bind_font(font=default_font)
+
+def server_starter():
+    asyncio.run(start_server())
+
+async def start_server():
+
+    try:
+        if Globals.ServerPath == "": Globals.ServerPath = filedialog.askopenfile().name
+    except: 
+        Globals.ConsoleText += "User canceled file selection\n"
+        dpg.set_value("console_text", Globals.ConsoleText)
+
+    Globals.LogPath = Globals.ServerPath.removesuffix("Server.exe") + "/Saved/Logs/OverlayLog.txt"
+
+    try:
+        Globals.ServerProc = await asyncio.subprocess.create_subprocess_exec(
+            Globals.ServerPath,f"{Globals.MapCode}?maxplayers={Globals.MaxPlayers}?daysbeforeblizzard={Globals.DaysUntilBlizzard}?dayminutes={Globals.DayMinutes}?predatordamage={Globals.PredatorDamage}?coldintensity={Globals.ColdIntensity}?hungerrate={Globals.HungerRate}?coalburnrate={Globals.CoalBurnRate}?thralls={Globals.ThrallCount} LOG=OverlayLog.txt nouniques"
+        )
+    except: 
+        Globals.ServerPath = ""
+        Globals.ConsoleText += "Server could not be started\n"
+        dpg.set_value("console_text", Globals.ConsoleText)
+
+    while not Globals.ServerProc.returncode:
+        with open(Globals.LogPath) as f: s = f.read()
+        Globals.ConsoleText = s
+        dpg.set_value("console_text", Globals.ConsoleText)
+
+        ScrollLimit = dpg.get_y_scroll_max(item="console_window")
+        dpg.set_y_scroll(item="console_window",value=ScrollLimit)
+
+        if dpg.is_key_pressed(dpg.mvKey_Escape):
+            break
+
+            
+    asyncio.run(kill_process())
+    Globals.ConsoleText = "Server Interupted"
+    dpg.set_value("console_text", Globals.ConsoleText)
+    
+
+def set_map(map: str):
+    if map == "Approach": 
+        Globals.MapCode = "Approach_Persistent"
+    elif map == "The Summit":
+        Globals.MapCode = "Departure_Persistent"
+    else: map = "Expanse_Persistent"
+
+def set_days_until_blizz(days:int):
+    Globals.DaysUntilBlizzard = days
+
+def set_day_mins(mins:int):
+    Globals.DayMinutes = mins
+
+def set_max_players(players:int):
+    Globals.MaxPlayers = players
+
+def set_thralls(thralls:int):
+    Globals.ThrallCount = thralls
+
+def set_pred_damage(preddamage:int):
+    Globals.PredatorDamage = preddamage
+
+def set_coal_burn_rate(burnrate:int):
+    Globals.CoalBurnRate = burnrate
+
+def set_cold_intensity(cold:int):
+    Globals.ColdIntensity= cold
+
+def set_hunger(hunger:int):
+    Globals.HungerRate = hunger
+
+with dpg.window(label="Console",width=WindowHeight,height=WindowWidth,no_close=True,tag="console_window"):
+    dpg.add_text(label="",tag="console_text",wrap=WindowWidth)
+
+with dpg.window(label="Start Server",width=WindowWidth,height=WindowHeight,no_close=True, on_close=save_prefs):
+
+    dpg.add_combo(items=("Approach","The Expanse","The Summit"),label="Select A Map",width=150,callback=set_map)
+    dpg.add_slider_int(label="Days Until Blizzard",default_value=3,min_value=1,max_value=10,width=150, callback=set_days_until_blizz)
+    dpg.add_slider_int(label="Day Minutes",default_value=8,min_value=1,max_value=50,width=150,callback=set_day_mins)
+    dpg.add_text("Player Settings")
+    dpg.add_slider_int(label="Max Players",default_value=8,min_value=1,max_value=8,width=150, callback=set_max_players)
+    dpg.add_slider_int(label="Thrall Count",default_value=3,min_value=1,max_value=8,width=150, callback=set_thralls)
+    
+    dpg.add_text("Difficulty Settings")
+
+    dpg.add_slider_float(label="Predator Damage",default_value=1,min_value=0,max_value=2,format='%.2f',width=150, callback=set_pred_damage)
+    dpg.add_slider_float(label="Coal Burn Rate",default_value=1,min_value=0,max_value=2,format='%.2f',width=150, callback=set_coal_burn_rate)
+    dpg.add_slider_float(label="Cold Intensity",default_value=1,min_value=0,max_value=2,format='%.2f',width=150, callback=set_cold_intensity)
+    dpg.add_slider_float(label="Hunger Rate",default_value=1,min_value=0,max_value=2,format='%.2f',width=150, callback=set_hunger)
+
+    dpg.add_button(label="Start Server", callback=server_starter)
+
+if not Globals.HasOpenedBefore:
+    with dpg.window(label="Getting Started",tag="getting_started",no_docking=True,no_collapse=True,autosize=True):
+        dpg.add_text("To get started, select the settings you would like then press start server and select your DreadHungerServer exectutable.\nTo stop the server, press the esc key.", wrap=500)
+        dpg.add_button(label="Close", callback=lambda: dpg.configure_item("getting_started", show=False))
+
+with dpg.theme() as global_theme:
+
+    with dpg.theme_component(dpg.mvAll):
+
+        dpg.add_theme_style(dpg.mvStyleVar_FrameRounding, 5)
+        dpg.add_theme_style(dpg.mvStyleVar_GrabRounding,5)
+        
+
+dpg.bind_theme(global_theme)
+
+dpg.setup_dearpygui()
+dpg.show_viewport()
+dpg.start_dearpygui()
+dpg.destroy_context()
